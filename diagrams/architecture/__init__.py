4b5a9ee335b62f756344de0ee0228458ae71a437
shiron <shiron4710.dev@gmail.com>
2023-07-28 09:36:40 +0900

    feat(architecture): add service fig

--- diagrams/architecture/__init__.py
+++ diagrams/architecture/__init__.py
@@ -1,4 +1,4 @@
-from diagrams import Diagram, Cluster
+from diagrams import Diagram, Cluster, Edge
 from diagrams.gcp.network import Armor
 from diagrams.gcp.compute import AppEngine, Run, Functions
 from diagrams.gcp.database import SQL, Datastore
@@ -8,37 +8,42 @@ from diagrams.gcp.analytics import Pubsub
 from diagrams.firebase.grow import Messaging
 from diagrams.firebase.base import Firebase
 from diagrams.generic.device import Mobile, Tablet
+from diagrams.onprem.database import PostgreSQL
+from diagrams.onprem.monitoring import Grafana
+from diagrams.programming.framework import Flutter, React
+from diagrams.programming.language import Python, Rust
 
-with Diagram("GCP Architecture", filename="imgs/gcp_architecture", show=False):
+
+def drawArchitecture(is_service=False):
     with Cluster("Client"):
-        visitor_mobile = Mobile("Visitor")
-        admin_tablet = Tablet("Admin")
+        visitor_mobile = Mobile("Visitor") if not is_service else Flutter("Visitor")
+        admin_tablet = Tablet("Admin") if not is_service else React("Admin")
 
     with Cluster("Google Cloud"):
         armor = Armor("Armor")
 
         with Cluster("Back-end API"):
-            backend_app = AppEngine("Back-end API")
-            backend_sql = SQL("Backend DB")
+            backend_app = AppEngine("Back-end API") if not is_service else Rust("Back-end API")
+            backend_sql = SQL("Backend DB") if not is_service else PostgreSQL("Backend DB")
             backend_nosql = Datastore("Backend Palette DB")
 
         with Cluster("Image Manage"):
             image_manage_run = Run("Image Manage System")
-            image_manage_db = SQL("Image Manage DB")
+            image_manage_db = SQL("Image Manage DB") if not is_service else PostgreSQL("Image Manage DB")
             image_storage = Storage("Image storage")
 
         with Cluster("Image Processing"):
-            image_clustering_run = Run("Image clustering")
-            image_gen_run = Run("Image generation")
+            image_clustering_run = Run("Image clustering") if not is_service else Python("Image clustering")
+            image_gen_run = Run("Image generation") if not is_service else Python("Image generation")
             image_pubsub = Pubsub("Image pub/sub")
 
         with Cluster("Monitoring"):
-            system_grafana_run = Run("System grafana")
+            system_grafana_run = Run("System grafana") if not is_service else Grafana("System grafana")
             monitoring = Monitoring("Cloud Monitoring")
 
         with Cluster("Event monitoring"):
-            event_grafana_run = Run("Event grafana")
-            user_grafana_func = Functions("Add user operation")
+            event_grafana_run = Run("Event grafana") if not is_service else Grafana("Event grafana")
+            user_grafana_func = Functions("Add user operation") if not is_service else Python("Add user operation")
 
     with Cluster("Firebase"):
         analytics = Firebase("Analytics")
@@ -79,3 +84,10 @@ with Diagram("GCP Architecture", filename="imgs/gcp_architecture", show=False):
         image_pubsub >> monitoring
 
     visitor_mobile >> analytics
+
+
+with Diagram("GCP Architecture", filename="imgs/gcp_architecture", show=False):
+    drawArchitecture()
+
+with Diagram("Service Architecture", filename="imgs/service_architecture", show=False):
+    drawArchitecture(is_service=True)
